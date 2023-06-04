314f447323efe57af92c603d1c3a50979054a6c8
Joao Vitor - sijoa <sijoa@Hawking-v3>
2023-06-03 21:01:54 -0300

    listados os módulos antigos e iniciada as conversões de nomes dos módulos para se equivalerem aos do sagelic. Adicionado comentários Em Oberservações para avaliarmos como serão tratados os módulos antigos que não possuem equivalencia com os atuais.

--- VarreLic.py
+++ VarreLic.py
@@ -4,8 +4,10 @@ import re
 lin = 0
 lin2 = 0
 
-try:
+modulo = [10]
+modulo = ["scd"]
 
+try:
  L = open('Licencas', 'r', encoding="utf-8")
 
 
@@ -19,36 +21,54 @@ try:
      print("Prazo: Permanente")
     else:
     # Comente o "continue para printar também lics temporárias"
-     continue
+     #continue
      print(f"Prazo: {lin[4]} dias")
     
     # Definindo codigo do cliente
-    try:
-     C = open('Clientes', 'r', encoding="utf-8")
+    C = open('Clientes', 'r', encoding="utf-8")
     
-     for linha2 in C:
-      lin2 = linha2.split(',')
-      if lin2[0] == lin[0] :
-       break
+    for linha2 in C:
+     lin2 = linha2.split(',')
+     if lin2[0] == lin[0] :
+      break
     
-     C.close()
+    C.close()
     
-    except Exception:
-     print("Arquivo de Clientes não encontrado ")
     # --------
     
     print(f"COD Cliente: {lin2[1]}")
     print(f"Licenciado: {lin[0]}")
     print(f"Site: {lin[1]}")
     print(f"ID: {lin[2]}")
-    print(f"Modulo: {lin[3]}")
-     
-    if int(lin[5]) == 2: 
-     print("Redundancia: Sim")
-    else:
-     print("Redundancia: Não")
+
+    #if lin[3] == "sageiccp"
+    # lin[3] = "iccp"
+    # print(f"Modulo: {lin[3]}")
+
+    #elif lin[3] == "sage61850"
+    # lin[3] = "61850"
+
+    #elif lin[3] == "sagesnmp"
+    # lin[3] = "snmp"
+
+    #print(f"Modulo: {lin[3]}")
+    #
+    #if int(lin[5]) == 2: 
+    # print("Redundancia: Sim")
+    #else:
+    # print("Redundancia: Não")
     
-    
+    # Listando módulos do banco
+    tamMod = len(modulo) - 1 
+    contador = 0
+    while tamMod >= contador:
+     if lin[3] != modulo[contador]:
+      if contador == tamMod:
+       modulo.append(lin[3])
+       break
+     elif lin[3] == modulo[contador]:
+      break
+     contador += 1
     # Definindo data de geração
     
     posi_data_init = re.search(lin2[1], lin[2]).end()
@@ -72,7 +92,8 @@ try:
     
  L.close()
 
-except Exception:
- print("Arquivo de \"Licenças\" ou \"Clientes\" não encontrado ou erro encontrado nos arquivos")
+except Exception as e :
+ print(f"Arquivo de \"Licenças\" ou \"Clientes\" não encontrado ou erro encontrado nos arquivos: {e}")
 else:
  print("Fim...")
+ print(f"Modulos identificados {modulo}")
